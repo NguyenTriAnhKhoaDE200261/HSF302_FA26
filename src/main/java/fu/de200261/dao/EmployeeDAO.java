14385c3121831f3814a0895a9e7b7ca5a9df35d7
Khoa <nguyentrianhkhoa2722006@gmail.com>
2026-09-11 22:21:09 +0700

    TODO 4

--- src/main/java/fu/de200261/dao/EmployeeDAO.java
+++ src/main/java/fu/de200261/dao/EmployeeDAO.java
@@ -11,16 +11,23 @@ public class EmployeeDAO {
     private static final EntityManagerFactory emf =
             Persistence.createEntityManagerFactory("hsf302FU");
 
-    // ---------- CREATE (TODO 0.3) ----------
-    public void save(Employee e) {
+
+
+    // ---------- READ (TODO 0.4) ----------
+    public Employee findById(Long id) {
         EntityManager em = emf.createEntityManager();
         try {
-            em.getTransaction().begin();
-            em.persist(e); // -> e chuyển sang MANAGED, sẽ được INSERT khi commit
-            em.getTransaction().commit();
-        } catch (RuntimeException ex) {
-            if (em.getTransaction().isActive()) em.getTransaction().rollback();
-            throw ex;
+            return em.find(Employee.class, id); // trả về null nếu không tồn tại
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Employee> findAll() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            return em.createQuery("SELECT e FROM Employee e", Employee.class)
+                    .getResultList();
         } finally {
             em.close();
         }
